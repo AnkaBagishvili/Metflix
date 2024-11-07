b7cf7d40581f9510b2bb827c4d248cd66d73a10b
AnkaBagishvili <lukiana.3@gmail.com>
2024-11-07 20:47:16 +0400

    add:signup page

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { FooterComponent } from '../../core/components/footer/footer.component';
 import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, LoginComponent],
+  imports: [HeaderComponent, FooterComponent, LoginComponent, SignupComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css',
 })
