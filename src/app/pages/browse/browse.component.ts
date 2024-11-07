2054217e2de1b04febadd7c12346f56035eb8227
AnkaBagishvili <lukiana.3@gmail.com>
2024-11-07 20:23:37 +0400

    add:login page

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { FooterComponent } from '../../core/components/footer/footer.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './browse.component.html',
-  styleUrl: './browse.component.css'
+  styleUrl: './browse.component.css',
 })
-export class BrowseComponent {
-
-}
+export class BrowseComponent {}
