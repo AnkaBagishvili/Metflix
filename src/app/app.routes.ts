e2cf2b9f86fa51df26070121354971f33f54e036
AnkaBagishvili <lukiana.3@gmail.com>
2024-11-19 20:30:09 +0400

    add:routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component:LoginComponent,
+  },
+  {
+    path:'registration',
+    component:SignupComponent,
+  }
+];
